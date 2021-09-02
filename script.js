8a0f53d9df29467dbcc7d523fbb32bcc9396ca69
KourtetDev <61595811+HogeyDev@users.noreply.github.com>
2021-09-02 07:26:10 -0400

    updated 9/2/2021

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight;
 
 class Player {
 	constructor() {
-		this.color = 'rgb(123, 50, 250)';
+		this.color = 'black';
 		this.radius = 15;
 		this.moveSpeed = 3;
 		this.x = 0;
@@ -24,12 +24,13 @@ class Player {
 				break;
 			case 3:
 				this.y += this.moveSpeed;
-				break;	
+				break;
 		}
 	}
 	draw() {
 		ctx.beginPath();
 		ctx.arc(canvas.width / 2, canvas.height / 2, this.radius, 0, 2 * Math.PI,  false);
+		ctx.fillStyle = this.color;
 		ctx.fill();
 		ctx.closePath();
 	}
@@ -46,31 +47,51 @@ class Map {
 		ctx.drawImage(image, this.x, this.y);
 	}
 }
+let reshoot = false;
 function render() {
-	for (let i = 0; i < move.length; i++) {
+	frames++
+	for (let i = 0; i < move.length - 1; i++) {
 		if (move[i]) {
 			player.move(i);
 		}
 	}
+	if (frames % 10 == 0) {reshoot = true}
+	if (move[4]) {bullets.push(new Bullet()); reshoot = false}
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	bg.draw(player, bgimage);
+	for (let i = 0; i < bullets.length; i++) {
+		bullets[i].draw({'x': player.x, 'y': player.y});
+		if (distance(bullets[i].x, bullets[i].y, bullets[i].orig[0], bullets[i].orig[1]) > bullets[i].distance) {
+			bullets.splice(i, 1)
+		}
+	}
 	player.draw();
+	ctx.font = '48px sans';
+	ctx.fillText(player.x + ', ' + player.y, 20, 50);
 }
 class Gun {
 	constructor() {
-		this.x 
+		// this.x 
 	}
 }
 class Bullet {
-	constructor()  {
+	constructor(speed, distance)  {
 		this.color = '#AF9B60';
-		this.x = 0;
-		this.y = 0;
+		this.speed = speed || 7;
+		this.x = player.x;
+		this.y = player.y;
+		this.orig = [player.x, player.y];
+		this.distance = distance || 2000;
+		this.angle = Math.atan2(-mouse.y, -mouse.x);
+		this.velocity = {'x': Math.cos(this.angle) * -this.speed, 'y': Math.sin(this.angle) * -this.speed}
 	}
-	draw(gun, mousePos) {
-		this.dir = Math.atan(mousePos[0])
-		this.relx = gun.x + this.x;
-		this.rely = gun.y + this.y;
+	draw(gun) {
+		this.x += this.velocity.x;
+		this.y += this.velocity.y;
+		this.relx = this.x - gun.x + canvas.width / 2;
+		this.rely = this.y - gun.y + canvas.height / 2;
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.relx, this.rely, 10, 10);
 	}
 }
 class Enemy {
@@ -78,18 +99,36 @@ class Enemy {
 }
 
 var player = new Player();
-var move = [false, false, false, false];
+var move = [false, false, false, false, false];
 const bgimage = document.getElementById('bg');
 var bg = new Map();
+var mouse = {};
+var bullets = [];
+var frames = 0;
 
-function setup() {
-	player.draw();
+function distance(x1, y1, x2, y2) {
+	return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
 document.onkeydown = function(e) {
-	move[e.keyCode - 37] = true;
-	// console.log(move);
+	if (e.key == 'a') {move[0] = true}
+	if (e.key == 'w') {move[1] = true}
+	if (e.key == 'd') {move[2] = true}
+	if (e.key == 's') {move[3] = true}
 }
 document.onkeyup = function(e) {
-	move[e.keyCode - 37] = false;
+	if (e.key == 'a') {move[0] = false}
+	if (e.key == 'w') {move[1] = false}
+	if (e.key == 'd') {move[2] = false}
+	if (e.key == 's') {move[3] = false}
 }
 setInterval(render, 10);
+document.onmousemove = function(e) {
+	mouse = {'x': e.clientX - canvas.width / 2, 'y': e.clientY - canvas.height / 2};
+}
+document.onmousedown = function(e) {
+	move[4] = true;
+}
+
+document.onmouseup = function(e) {
+	move[4] = false;
+}
